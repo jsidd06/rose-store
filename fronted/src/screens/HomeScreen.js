bc018f991ec841bb0a786e2bc7edc78544b96292
siddharth <jsidd06@gmail.com>
2023-02-01 16:41:17 +0530

    add loading box and messagebox

--- fronted/src/screens/HomeScreen.js
+++ fronted/src/screens/HomeScreen.js
@@ -2,25 +2,38 @@ import React, { useEffect, useState } from "react";
 import Product from "../components/Product";
 import * as EndPoint from "../Api/services";
 import Axios from "../Api/Axios";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 export default function HomeScreen() {
   const [allDataProduct, setAllDataProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     Axios.get(EndPoint.ALL_PRODUCT_LIST)
       .then((res) => {
         setAllDataProduct(res.data);
+        setLoading(false);
       })
       .catch((err) => {
-        console.log(err);
+        console.log(err.message);
+        setLoading(false);
       });
   }, []);
 
   return (
     <div className="row center">
-      {allDataProduct.map((product) => (
-        <Product product={product} key={product._id} />
-      ))}
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger"> {error}</MessageBox>
+      ) : (
+        allDataProduct.map((product) => (
+          <Product product={product} key={product._id} />
+        ))
+      )}
     </div>
   );
 }
